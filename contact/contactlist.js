262443a29e90d1e675c64aee1d15a21dd0be4abc
kailash1805 <kailashsuthar1805@gmail.com>
2017-08-17 22:39:49 +0530

    first commit

--- contact/contactlist.js
+++ contact/contactlist.js
@@ -7,36 +7,113 @@
  */
 var objMasterContact = new MasterContact();
 
-var Contact = function () { };
+var ContactMaster = function () { };
 
 /**
- * prototypes properties
+ * init function for onload application functionality
  */
-Contact.prototype.setContactDetails = function (detailsInfo) {
-    console.log(detailsInfo);
+ContactMaster.init = function () {
+    
+    var contactSearch = document.getElementById('contact-search');
+    contactSearch.addEventListener('keyup', function (event) {
+        var customList = [];
+        var contactListInformation = JSON.parse(localStorage.getItem('contacts'));
+        
+        var buildingListContainer = document.getElementById('sideList');
+
+        if (event.target.value != "") {
+            
+            contactListInformation.results.forEach(function (currentItem, index) {
+                if (currentItem.name.first.indexOf(event.target.value) !== -1) {
+                    customList.push(currentItem);
+                }
+            });
+
+            // build custom contact list 
+            ContactMaster.buildContactList(customList, buildingListContainer);
+        }else {
+            
+            // build all contact list 
+            ContactMaster.buildContactList(contactListInformation.results, buildingListContainer);
+        }
+    });
 }
 
-Contact.prototype.makeDOMList = function (contactList, rootElement) {
+/**
+ * Build contact list
+ */
+ContactMaster.buildContactList = function(contactList, rootElement){
+
+    rootElement.innerHTML = "";
+
     var ul = document.createElement('ul'),
         li = document.createElement('li'),
+        img = document.createElement('img'),
+        span = document.createElement('span'),
         list = document.createDocumentFragment();
 
-    for (contact in contactList) {
-        if (contactList.hasOwnProperty(contact)) {
-            debugger;
+    ul.className = "list-group";
+
+    contactList.forEach(function (item, index) {
+        var cloneImg = img.cloneNode(false);
+        cloneImg.setAttribute('src', item.picture.thumbnail);
+        cloneImg.className = "img-circle";
+
+        var cloneLI = li.cloneNode(false);
+        cloneLI.setAttribute('id', item.phone);
+
+        cloneLI.className = "list-group-item";
+        var nameSpan = span.cloneNode(false);
+        nameSpan.setAttribute('id', item.phone);
+        nameSpan.appendChild(document.createTextNode(item.name.first));
+        cloneLI.appendChild(cloneImg);
+        cloneLI.appendChild(nameSpan);
+        list.appendChild(cloneLI);
+    })
+
+    ul.appendChild(list);
             
+    rootElement.appendChild(ul);
+
+    rootElement.addEventListener('click', function (event) {
+        if (event.target) {
+            var sideDetails = document.getElementById("sideDetails");
+            sideDetails.style.display = 'block';
+            var uniqueId = event.target.id;
+            // get localstorage contact list
+            var contactListInformation = JSON.parse(localStorage.getItem('contacts'));
+            contactListInformation.results.forEach(function (currentItem, index) {
+                if (currentItem.phone == uniqueId) {
+                    document.getElementById('contactName').textContent = currentItem.name.first + " " + currentItem.name.last;
+                    document.getElementById('contactLocation').textContent = currentItem.location.street + " " + currentItem.location.city + " " + currentItem.location.state + " " + currentItem.location.postcode;
+                    document.getElementById('email').textContent = currentItem.email;
+                    document.getElementById('contactPhone').textContent = currentItem.phone;
+                    document.getElementById('contactNat').textContent = currentItem.nat;
+                    document.getElementById('contactPicture').setAttribute('src', currentItem.picture.medium);
+                }
+            });
         }
-    }
+    });
 }
 
 
-Contact.prototype = objMasterContact;
-var objContact = new Contact();
-objContact.getList(function (list) {
-    console.log(list);
-    var contactSideList = "";
-    if (list != undefined) {
-        objContact.makeDOMList(list, document.getElementById("side-list"));
-    }
+ContactMaster.prototype = Object.create(MasterContact.prototype);
+var objContactmaster = new ContactMaster();
 
+/**
+ * on load application init function calling
+ */
+ContactMaster.init();
+
+/**
+ * on load get contact list API call
+ */
+objContactmaster.getList(function (contactList) {
+    var buildingListContainer = document.getElementById('sideList');
+
+    /**
+     * build contact list DOM
+     */
+    ContactMaster.buildContactList(contactList.results, buildingListContainer);
 });
+
--- master/MasterContact.js
+++ master/MasterContact.js
@@ -2,10 +2,7 @@
  * Author: Kailash kumar
  */
 
-/**
- * create configuration class object
- */
-var config = new Config();
+var objConfig = new Config();
 
 var MasterContact = function () {
     this.list = [];
@@ -18,10 +15,10 @@ MasterContact.prototype.getList = function (callback) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            this.list = JSON.parse(this.responseText);
-            callback(this.list);
+            localStorage.setItem('contacts', this.responseText);
+            callback(JSON.parse(this.responseText));
         }
     };
-    xhttp.open("GET", config.BASE_URL + "?results=10", true);
+    xhttp.open("GET", objConfig.BASE_URL + "?results=10", true);
     xhttp.send();
 }
